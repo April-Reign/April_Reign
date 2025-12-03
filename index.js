284a40dd8b54f1ec70ddc9ff150fca523e44689d
April-Reign <harrison.april@ymail.com>
2025-12-02 19:29:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,3 +31,26 @@ let maintenanceCalories = tdee + caloriesBurned;
         "Maintenance Calories " + maintenanceCalories.toFixed(2);
     
         };
+
+
+
+
+/*Counter program continued from Websitestyle.css*/
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
+/*End of counter program*/
