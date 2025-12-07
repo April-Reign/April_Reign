f8293235098a0163983d7d2abc69113c63b7a6d0
April-Reign <harrison.april@ymail.com>
2025-12-07 18:51:26 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,59 @@
-/*
-Hack it: Zombie Attack
+//global variables that will store the toolbox colour palette
+//amnd the helper functions
+var toolbox = null;
+var colourP = null;
+var helpers = null;
 
-The program creates a horde of zombies that cross the canvas. Read the code
-and make the following enhancements
+//spray can object literal
+sprayCan = {
+    name: "sprayCanTool",
+    icon: "assets/sprayCan.jpg",
+    points: 13,
+    spread: 10,
+    draw: function(){
+        //if the mouse is pressed paint on the canvas
+        //spread describes how far to spread the paint from the mouse pointer
+        //points holds how many pixels of paint for each mouse press.
+        if(mouseIsPressed){
+            for(var i = 0; i < this.points; i++){
+                point(random(mouseX-this.spread, mouseX + this.spread), 
+                    random(mouseY-this.spread, mouseY+this.spread));
+            }
+        }
+    }
+};
 
-1. Split the code up into muliple files one for each constructor
-2. Give each zombie a health property
-3. Add a shovel! When the zombie is clicked on the head their health decreases
-	* Add a 'clicked' method to the zombie to check if x and y coordinates, taken as arguments
-	 are over the zombies head (you can use the dist function) and decrease health
-	* In the horde constructor create a new method to check each zombies clicked method
-	* also remove from the array any zombies whoes health is below 0.
-	* call the method in horde from mousePressed();
-4. When a zombie is killed make sure a new zombie is added to the horde.
-5. Extension: Make it a game where the idea is to keep the zombies from the right of the screen
-for as long as possible.
-*/
+function setup() {
 
-//variable to store the zombie horde
-var horde;
+	//create a canvas to fill the content div from index.html
+	canvasContainer = select('#content');
+	var c = createCanvas(canvasContainer.size().width, canvasContainer.size().height);
+	c.parent("content");
 
-function mousePressed() {
-    horde.checkClicks(mouseX, mouseY);
+	//create helper functions and the colour palette
+	helpers = new HelperFunctions();
+	colourP = new ColourPalette();
+
+	//create a toolbox for storing the tools
+	toolbox = new Toolbox();
+
+	//add the tools to the toolbox.
+	toolbox.addTool(new FreehandTool());
+	toolbox.addTool(new LineToTool());
+	toolbox.addTool(sprayCan);
+	toolbox.addTool(new mirrorDrawTool());
+	background(255);
+
 }
 
+function draw() {
+	//call the draw function from the selected tool.
+	//hasOwnProperty is a javascript function that tests
+	//if an object contains a particular method or property
+	//if there isn't a draw method the app will alert the user
+	if (toolbox.selectedTool.hasOwnProperty("draw")) {
+		toolbox.selectedTool.draw();
+	} else {
+		alert("it doesn't look like your tool has a draw method!");
+	}
+}
